76148187d05bb6fd0fb9947b7b4e9c42a1b28041
dvhata <thaohanh233@gmail.com>
2021-10-29 02:25:11 +0700

    Sign in and Sign up, not css

--- src/components/FormSign/FormSign.tsx
+++ src/components/FormSign/FormSign.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import SignIn from './SignIn/SignIn'
+import SignUp from './SignUp/SignUp'
 
 export default function FormSign() {
     return (
         <div>
-            
+            <SignIn />
+            <SignUp />
         </div>
     )
 }
